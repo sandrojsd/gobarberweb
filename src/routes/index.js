9acf6eba32123a8bbe2915a43d53f373555fd7cb
Alexsandro Trindade <sandrojsd@gmail.com>
2020-05-16 13:51:26 -0400

    Configuração das rotas

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+
+import SignIn from '../pages/SignIn';
+import SingUp from '../pages/SignUp';
+import Dashborad from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SingUp} />
+
+      <Route path="/dashboard" component={Dashborad} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+    </Switch>
+  );
+}
